6a561c1800be299a200cab4f7d5c28f0512b27fd
scy <13057160969@163.com>
2022-05-23 01:05:47 +0800

    获取用户信息和code

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,21 +1,11 @@
 <template>
 	<view>
-		<!-- 登录 -->
-		<view class="login-box">
-			<view class="login-btn">手机号码一键注册登录</view>
-		</view>
-		<!-- 条款与政策 -->
-		<view class="clause">
-			<!-- 复选框 -->
-			<view class="checkbox">
-				<van-checkbox :value="checked" checked-color="#07c160" @change="checkClause" />
-			</view>
-			<!-- 文本 -->
-			<view class="text">我已阅读并同意
-				<navigator class="clause-title" url="/pages/detailManage/serviceClauseDetail">《工易谷服务条款》</navigator>及
-				<navigator class="clause-title" url="/pages/detailManage/privacyPolicyDetail">《隐私政策》</navigator>
-			</view>
-		</view>
+		<navigator url="/pages/login/login">
+			<button>登录</button>
+		</navigator>
+		<button type="primary" open-type="getUserInfo" @getUserInfo="getUserInfo">
+		  微信用户一键登录
+		</button>
 	</view>
 </template>
 
@@ -23,48 +13,73 @@
 	export default {
 		data() {
 			return {
-				checked: false
 			}
 		},
+		onLoad() {
+			console.log(11)
+			this.getUserInfo()
+		},
 		methods: {
-			checkClause() {
-				console.log(1)
+			getUserInfo() {
+				var self = this;
+				// 获取用户信息
+				console.log(self)
+					uni.login({
+						provider: 'weixin',
+						success: function(res) {
+							console.log(12)
+							console.log(res)
+							// if (res.code) {
+							// 	//发起网络请求
+							// 	uni.request({
+							// 		method: 'POST',
+							// 		url: 'http://************/wxlogin',
+							// 		data: {
+							// 			code: res.code
+							// 		},
+							// 		success(res) {
+							// 			//将openid存入本地缓存
+							// 			// uni.setStorage({
+							// 			// 	key: 'openid_key',
+							// 			// 	data: res.data.openid
+							// 			// });
+							// 			// if (res.statusCode == 200 && res.data && res.data.username) {
+							// 			// 	self.isFirstWXLogin = false;
+							// 			// 	self.name = res.data.username;
+							// 			// 	self.password = res.data.password;
+							// 			// 	setTimeout(function() {
+							// 			// 		self.tologin({
+							// 			// 			username: res.data.username,
+							// 			// 			password: res.data.password,
+							// 			// 			encrypted: true
+							// 			// 		})
+							// 			// 	}, 0)
+							// 			// } else {
+							// 			// 	//首次登录，可以跳转到一个绑定账号的页面
+							// 			// 	uni.navigateTo({
+							// 			// 		url: 'wxlogin'
+							// 			// 	});
+							// 		 //    }
+							// 		}
+							// 	})
+							// } else {
+							// 	console.log('登录失败！' + res.errMsg)
+							// }
+						},
+						fail(e) {
+			                console.log('失败')
+							console.log(e);
+						},
+						complete(e) {
+							console.log('完成')
+							console.log(e);
+						}
+				})
 			}
 		}
 	}
 </script>
 
 <style lang="scss">
-.login-box {
-	margin-top: 350rpx;
-	width: 100%;
-}
-.login-btn {
-	text-align: center;
-	height: 90rpx;
-	line-height: 90rpx;
-	width: 656rpx;
-	background-color: #07c160;
-	color: #fff;
-	margin: 0 auto;
-	border-radius: 42rpx;
-}
-.clause {
-	display: flex;
-	margin: 0 auto;
-	margin-top: 30rpx;
-	width: 656rpx;
-}
-.checkbox {
-	margin-top: 2rpx;
-}
-.text {
-	font-size: 28rpx;
-	color: #777676;
-	line-height: 48rpx;
-}
-.clause-title {
-	color: #2196F3;
-	display: inline;
-}
+
 </style>
